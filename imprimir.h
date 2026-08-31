251955f079404793f9eabcb59291bbe75cb845ef
ricardosaulvb-png <ricardosaulvb@gmail.com>
2026-08-30 20:15:51 -0500

    Add imprimir.h with print utility functions

---
+++ imprimir.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+
+string msg = "";
+int num = 0;
+float dec = 0.0;
+double dbl = 0.0;
+bool logic = false;
+
+inline void PRINTs(string msg) {
+	cout << msg << endl;
+}
+
+inline void PRINTi(int num) {
+	cout << num << endl;
+}
+
+inline void PRINTb(bool logic) {
+	cout << boolalpha << logic << endl;
+}
+
+inline void PRINTf(float dec) {
+	cout << dec << endl;
+}
+
+inline void PRINT_d(double dbl) {
+	cout << dbl << endl;
+}
