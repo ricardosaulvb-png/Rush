5ac516177570d37b17aaebee816fc31b250e7da5
ricardosaulvb-png <ricardosaulvb@gmail.com>
2026-08-30 20:19:13 -0500

    Create Rush.h header file with includes

---
+++ Rush.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include "imprimir.h"
+#include "inputRush.h"
+#include "time.h"
+#include "RandomRush.h"
+#include "operaciones_basicas.h"
+
+using namespace std;
