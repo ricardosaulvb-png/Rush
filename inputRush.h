f41689286206ead54dd35d0d3b5c2d79b06c67c9
ricardosaulvb-png <ricardosaulvb@gmail.com>
2026-09-09 10:58:47 -0500

    Refactor input reading functions in inputRush.h

--- inputRush.h
+++ inputRush.h
@@ -1,12 +1,25 @@
 #pragma once
 #include <iostream>
+#include <string_view>
 
+struct IOSetup {
+    IOSetup() {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+    }
+};
+
+inline IOSetup io_setup;
 
 /// INPUT function that reads the given arguments from the console.
 
 template<typename... Args>
-inline void read(const char* prompt, Args&... args) {
+inline void readtxt(const char* prompt, Args&... args) {
 	std::cout << prompt;
 	(std::cin >> ... >> args);
 }
 
+template<typename... Args>
+inline void read(Args&... args) {
+	(std::cin >> ... >> args);
+}
