a5e2e4a5bb781d9ef82d2352376d3b7e8ecfba63
ricardosaulvb-png <ricardosaulvb@gmail.com>
2026-09-09 10:57:11 -0500

    Add IOSetup struct for console I/O optimization

--- console_print.h
+++ console_print.h
@@ -1,5 +1,16 @@
 #pragma once
 #include <iostream>
+#include <string_view>
+
+struct IOSetup {
+    IOSetup() {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+    }
+};
+
+inline IOSetup io_setup;
+
 ///  PRINT function that prints the given arguments to the console, followed by a newline.
 
 template<typename... Args>
